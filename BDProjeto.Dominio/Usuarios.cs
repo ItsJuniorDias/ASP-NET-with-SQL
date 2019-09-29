2e0307c0e25e33391e3c756636435ea1345165d8
Junior Dias <juniordias_@live.com>
2019-09-29 19:30:55 -0300

    Upload Dominion

---
+++ BDProjeto.Dominio/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDProjeto.Dominio
+{
+    public class Usuarios
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string cargo { get; set; }
+        public DateTime data { get; set; }
+    }
+}
