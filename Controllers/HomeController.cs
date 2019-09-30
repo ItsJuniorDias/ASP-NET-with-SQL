a3311892fba39f7052296ce03f85c165d6ec275e
Junior Dias <juniordias_@live.com>
2019-09-29 23:58:38 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BDProjeto.Aplicacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public ActionResult Index()
+        {
+           
+            return View();
+        
+        }
+
+        public ActionResult Index1()
+        {
+
+            return WebFormViewEngine();
+
+        }
+
+        private ActionResult WebFormViewEngine()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
